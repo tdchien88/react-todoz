149a678079a2d1394958f4cd00cbd60360f24084
TDChien88 <tdchien88@gmail.com>
2020-02-07 02:46:19 +0900

    Updates

---
+++ precache-manifest.3dfd7b7155da57fecfab7c0d854e0736.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a57d1b862d6816233b179031e112e072",
+    "url": "/react-todoz/index.html"
+  },
+  {
+    "revision": "7d1c6f11f069e5a1eca2",
+    "url": "/react-todoz/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "35b3b1c91d71b182a0b3",
+    "url": "/react-todoz/static/js/2.2fe87da1.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-todoz/static/js/2.2fe87da1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d1c6f11f069e5a1eca2",
+    "url": "/react-todoz/static/js/main.9cebc608.chunk.js"
+  },
+  {
+    "revision": "073746a79a7c708a7ff4",
+    "url": "/react-todoz/static/js/runtime-main.eb641a1d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-todoz/static/media/logo.5d5d9eef.svg"
+  }
+]);
